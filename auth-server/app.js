6c47b244d0e8764fcdd9e77e66abb7704253b968
Brennan Payne <brennan.s.payne@gmail.com>
2017-11-08 15:20:27 -0600

    Very basic auth

--- auth-server/app.js
+++ auth-server/app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session')
+var cors = require('cors');
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,9 +21,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', index);
-app.use('/users', users);
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'secretsecret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000 }
+}));
+
+app.get('/check', function(req, res) {
+  if (req.session.isAuthenticated) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(401);
+  }
+});
+app.post('/authenticate', function(req, res) {
+  req.session.isAuthenticated = true;
+  req.session.save(function(){
+    console.warn(req.session);
+    res.sendStatus(200);
+  })
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
